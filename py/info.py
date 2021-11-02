68f1d51bb818e4b0759d55e9d737ad8448808104
The Info Bots <83772959+theinfobots@users.noreply.github.com>
2021-11-02 18:51:42 +0100

    Add files via upload

---
+++ py/info.py
@@ -0,0 +1,45 @@
+fname = input("What's your first name? ")
+
+if fname == fname:
+    print("OK")
+    
+lname = input("What's your last name? ")
+
+if lname == lname:
+    print("OK")
+
+age = input("What is your age? ")
+
+if age == age:
+    print("OK")
+
+lang = input("How tall are you? ")
+
+if lang == lang:
+    print("OK")
+
+eye = input("What eyecolour do you have? ")
+
+if eye == eye:
+    print("OK")
+
+land = input("Which country do you live in? ")
+
+if land == land:
+    print("OK")
+
+region = input("Which county do you live in? ")
+
+if region == region:
+    print("OK")
+
+stad = input("What city do you live in? ")
+
+if stad == stad:
+    print("OK")
+
+print("Your name is: " + fname + " " + lname)
+print("Your are: " + age + " & " + lang)
+print("You have eye color: " + eye)
+print("Country you live in: " + land)
+print("County/municipality you live in: " + region + " & " + stad)
