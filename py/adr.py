8dcbf93cb5b110d2a4a4277026ea7043e67beb32
The Info Bots <83772959+theinfobots@users.noreply.github.com>
2021-11-02 19:53:33 +0100

    Add files via upload

---
+++ py/adr.py
@@ -0,0 +1,23 @@
+while True:
+    V = "0.1.3"
+    print("Version:" + V + " / Welcome!")
+    yes_no = input("Run this program? (y/n):")
+    if yes_no == "Nn":
+        print("OK, Well done..")
+        break
+    elif yes_no == "y":
+        print("Running now.")
+        fname = input("Whats is your name?: ")
+        if fname == "Alexander":
+            print("We have the same name.")
+        elif fname == "Niklas":
+            print("Know someone named niklas.")
+        elif fname == "":
+            print("[ ? ] You must answer.")
+        else:
+            print("What a nice name you have " + fname + "! Have a nice day.")
+        break
+    else:
+        break
+        print("Skriva y or n.")
+        
