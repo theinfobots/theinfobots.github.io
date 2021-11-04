924f88e5b0bf76d9a23753d55bef55bd85f96e6d
The Info Bots <83772959+theinfobots@users.noreply.github.com>
2021-11-04 20:06:29 +0100

    Add files via upload

--- linux/core-inc/config.sh
+++ linux/core-inc/config.sh
@@ -1,2 +1,33 @@
 #!/bin/bash
 
+# Add colors options & font text options
+export ITALIC='\e[3m'
+export B='\e[1m'
+export DIM='\e[2m'
+export UNDERLINE='\e[4m'
+export BLINK='\e[5m'
+export RESET_ALL='\e[0m'
+export REVERSE='\e[7m' #(invert the foreground and background colors)
+export HIDDEN='\e[7m' #(useful for passwords)
+
+export COLOR_NC='\e[0m' # No Color
+export COLOR_BLACK='\e[0;30m'
+export COLOR_GRAY='\e[1;30m'
+export COLOR_RED='\e[0;31m'
+export COLOR_LIGHT_RED='\e[1;31m'
+export COLOR_GREEN='\e[0;32m'
+export COLOR_LIGHT_GREEN='\e[1;32m'
+export COLOR_BROWN='\e[0;33m'
+export COLOR_YELLOW='\e[1;33m'
+export COLOR_BLUE='\e[0;34m'
+export COLOR_LIGHT_BLUE='\e[1;34m'
+export COLOR_PURPLE='\e[0;35m'
+export COLOR_LIGHT_PURPLE='\e[1;35m'
+export COLOR_CYAN='\e[0;36m'
+export COLOR_LIGHT_CYAN='\e[1;36m'
+export COLOR_LIGHT_GRAY='\e[0;37m'
+export COLOR_WHITE='\e[1;37m'
+
+export V='2.4.1'
+
+user_log=$(logname)
